1623f6fc996c73cfd41dc172d5a71f7c1b95e4e1
Pengustus <orlin.rizov.bg@gmail.com>
2026-02-10 08:59:23 +0200

    Add project files.

---
+++ WebServer.Server/HTTP/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace WebServer.Server.HTTP_Request
+{
+    public class Response
+    {
+        public StatusCode StatusCode { get; init; }
+        public HeaderCollection Headers { get;  } = new HeaderCollection();
+        public string Body { get; set; }
+        public Response(StatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+
+            this.Headers.Add("Server", "My Web Server");
+            this.Headers.Add("Data", $"{DateTime.UtcNow:r}");
+        }
+    }
+}
---
+++ WebServer.Server/HTTP/StatusCode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer.Server.HTTP_Request
+{
+    public enum StatusCode
+    {
+        OK = 200
+    }
+}
